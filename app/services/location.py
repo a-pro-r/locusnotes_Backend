cee8331f61ad1988716a31c5183fae3567c61352
aparokshith <aparokshith1997@gmail.com>
2025-01-18 14:33:11 -0700

    Initial commit: Basic API Structure

---
+++ app/services/location.py
@@ -0,0 +1,10 @@
+class LocationService:
+    @staticmethod
+    async def update_location(latitude: float, longitude: float):
+        # Implementation for updating location
+        pass
+
+    @staticmethod
+    async def get_nearby_notes(radius: float):
+        # Implementation for getting nearby notes
+        pass
