4d9ca589a88ddc6e27cd1e867a0662f2ec54d12b
aparokshith <aparokshith1997@gmail.com>
2025-01-20 16:01:06 -0700

    update note + tests

--- app/tests/db_tests.py
+++ app/tests/db_tests.py
@@ -59,8 +59,36 @@ async def get_note_by_userid():
         print(f"❌ Error: {str(e)}")
 
 
+async def test_update_note():
+    note_id = "ad83213a-8554-4ca3-93f1-086686e672d9"
+    url = f'http://localhost:8000/api/v1/notes/{note_id}'
+
+    updated_note = {
+        "user_id": "7dd28284-477d-48b2-b65e-d2e4b62d2a0d", # Test User ID
+        "title": "Updated Test Note @ walmart",
+        "content": "Updated Test Content",
+        "tags": ["test", "updated"],
+        "location_name": "Updated Test Location",
+        "latitude": 40.7128,
+        "longitude": -74.0060,
+        "address": "123 Updated Test St"
+    }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            print(f"\nTrying to update note...")
+            async with session.put(url, json=updated_note) as response:
+                status = response.status
+                result = await response.json()
+                print(f"Status Code: {status}")
+                print(f"Response: {result}")
+    except Exception as e:
+        print(f"❌ Error: {str(e)}")
+
+
 if __name__ == "__main__":
     print("Starting API tests...")
     # asyncio.run(test_create_note())
     # asyncio.run(get_note_by_id())
-    asyncio.run(get_note_by_userid())
+    # asyncio.run(get_note_by_userid())
+    asyncio.run(test_update_note())
